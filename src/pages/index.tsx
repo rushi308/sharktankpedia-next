37c1b8ec24cc01968803ec3cd9b4f87a6b367f2f
Rushi Patel <patel.rus30@gmail.com>
2023-02-14 22:19:12 +0000

    sorting for recent products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,35 +4,44 @@ import { getProducts } from "@/utils/api/client";
 import { Product } from "@/schema";
 
 type HomeProps = {
-  featuredProducts:Product[],
-  recentProducts:Product[]
-}
+  featuredProducts: Product[];
+  recentProducts: Product[];
+};
 
-export default function Home({featuredProducts,recentProducts}:HomeProps) {
+export default function Home({ featuredProducts, recentProducts }: HomeProps) {
   return (
     <>
-        <HomePage featuredProducts={featuredProducts} recentProducts={recentProducts} />
+      <HomePage
+        featuredProducts={featuredProducts}
+        recentProducts={recentProducts}
+      />
     </>
   );
 }
-export async function getServerSideProps({req,res}:any) {
+export async function getServerSideProps({ req, res }: any) {
   // Call an external API endpoint to get products
-  try{
+  try {
     res.setHeader(
-      'Cache-Control',
-      'public, s-maxage=10, stale-while-revalidate=59'
-    )
+      "Cache-Control",
+      "public, s-maxage=10, stale-while-revalidate=59"
+    );
     const featuredProducts = await getProducts(NaN, true);
-    const recentProducts = await getProducts(1000, false);
+    const recentProductsResponse = await getProducts(1000, false);
+    // Sort By episode as of now
+    let recentProducts: Product[] = [];
+    if (recentProductsResponse.products.length > 0) {
+      recentProducts = recentProductsResponse.products.sort((a, b) => {
+        return(a.season - b.season || b.episode - a.episode  )
+      });
+    }
     return {
       props: {
         featuredProducts: featuredProducts.products,
-        recentProducts: recentProducts.products
+        recentProducts: recentProducts,
       },
     };
   } catch (e) {
-    console.log(e)
+    console.log(e);
     throw new Error("Error while fetching graphql product data");
   }
-  
 }
