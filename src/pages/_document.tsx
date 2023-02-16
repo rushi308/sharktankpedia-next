f94c5b7413c9dcc997280b0902b1f5e1f83b097d
Rushi Patel <patel.rus30@gmail.com>
2023-02-16 22:37:54 +0000

    google ads

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,17 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Script>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7553767508353117"
+          crossOrigin="anonymous"
+        ></script>
+      </Script>
       <body>
         <Main />
         <NextScript />
